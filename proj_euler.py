740f4115ca0110508f0b45916a3140938bdbea71
Yoseph Borai <yborai@logicworks.net>
2017-06-07 17:44:37 +0000

    Upload new file

---
+++ proj_euler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def is_prime(num):
+    for c in range(2, num):
+        if num%c == 0:
+            return False
+    return True
+
+max = 0
+num = 600851475143
+
+for c in range(2, num):
+    if is_prime(c):
+        while((num%c) == 0):
+            num = num/c
+            max = c
+            print(c, num)
+        
+print(max)
