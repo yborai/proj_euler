d163ff36ea77ce41dd006c25043af5cd09e79485
Yoseph Borai <yborai@logicworks.net>
2017-06-07 17:44:53 +0000

    Upload new file

---
+++ proj_euler5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+min = 20
+flag = False
+
+for c in range(2520, 1000000000, 20):
+	for n in range(20, 0, -1):
+		if not(c%n == 0):
+			if n < min:
+				min = n
+
+
+			print(n)
+			break
+
+		if n == 1:
+			flag = True
+			print('c*n=', c*n)
+
+	if flag == True:
+		break
+
+print(min)
