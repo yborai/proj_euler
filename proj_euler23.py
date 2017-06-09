3c3a1f71e653393e73a72d9b6aaf467d25828ea7
Yoseph Borai <yborai@logicworks.net>
2017-06-09 13:25:28 +0000

    Upload new file

---
+++ proj_euler23.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from functools import reduce
+
+def get_factors(num):
+	return [
+		[i, num//i]
+		for i in range(1, int(num**0.5) + 1)
+		if num % i == 0
+	]
+
+def is_abundant(num): 
+	dude = set(reduce(
+		list.__add__,
+		get_factors(num)
+	))
+	if (sum(dude) - num) > num:
+		return True
+	else:
+		return False
+
+def is_sum(num, guy):
+	for val in guy:
+		if val > (num / 2):
+			return False
+
+		if (num - val) in guy:
+			print(num)
+			return True
+
+	return False
+
+if __name__ == "__main__":
+	guy = []
+	total = 0
+	
+	for n in range(12, 28123): 
+		if is_abundant(n):
+			guy.append(n)
+
+	print(guy) 
+
+	for c in range(20161):
+		if not is_sum(c, guy):
+			total += c
+
+	print(total)
