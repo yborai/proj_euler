c9dbdc91ec29b8e117b2938ddbe5040aa31746e2
Yoseph Borai <yborai@logicworks.net>
2017-06-09 21:02:08 +0000

    Replace proj_euler5.py

--- proj_euler5.py
+++ proj_euler5.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-min = 20
-flag = False
+import time
 
-for c in range(2520, 1000000000, 20):
-	for n in range(20, 0, -1):
-		if not(c%n == 0):
-			if n < min:
-				min = n
+if __name__ == "__main__":
+	start_time = time.time()
+	min = 20
+	flag = False
+
+	for c in range(2520, 1000000000, 20):
+		for n in range(20, 0, -1):
+			if not(c%n == 0):
+				if n < min:
+					min = n
 
 
-			print(n)
+				print(n)
+				break
+
+			if n == 1:
+				flag = True
+				print('c*n=', c*n)
+
+		if flag == True:
 			break
 
-		if n == 1:
-			flag = True
-			print('c*n=', c*n)
-
-	if flag == True:
-		break
-
-print(min)
+	print(min)
+	print(time.time() - start_time)
