b00e28c340be8ff3371a562c0f884ec0f3a89e08
Yoseph Borai <yborai@logicworks.net>
2017-06-09 21:02:23 +0000

    Replace proj_euler6.py

--- proj_euler6.py
+++ proj_euler6.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import time
 
-sosq = 0
-sqos = 0
+if __name__ == "__main__":
+	start_time = time.time()
+	sosq = 0
+	sqos = 0
 
-for c in range(1, 101):
-	sosq += c**2
-	sqos += c
+	for c in range(1, 101):
+		sosq += c**2
+		sqos += c
 
-print("difference = ", (sqos**2 - sosq))
+	print("difference = ", (sqos**2 - sosq))
+	print(time.time() - start_time)
