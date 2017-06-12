eb03738bd996f9d80bb82734ff13be9c16d93f9f
Yoseph Borai <yborai@logicworks.net>
2017-06-12 16:33:41 -0400

    fixed merge conflict

--- proj_euler.py
+++ proj_euler.py
@@ -8,7 +8,6 @@ def is_prime(num):
     return True
 
 
-<<<<<<< HEAD
 def prime_time(num):
 	max = 0
 
@@ -18,19 +17,8 @@ def prime_time(num):
 	            num = num/c
 	            max = c
 	            if(num == 1):
-	            	print(max)
 	            	return(max)
-=======
-if __name__ == "__main__":
-	max = 0
-	num = 600851475143
 
-	for c in range(2, num):
-	    if is_prime(c):
-	        while((num%c) == 0):
-	            num = num/c
-	            max = c
-	            print(c, num)
-	        
-	print(max)
->>>>>>> 9d957983ad66b566d2043c720b6203b4d3de6ef1
+
+if __name__ == "__main__":
+	print(prime_time(600851475143))
